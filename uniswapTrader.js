07ef31dde48560d50eb57dc668ee46b8417037d2
Abhshek Kumar <abui8994ty@gmail.com>
2024-04-04 20:02:41 +0000

    intial setup

---
+++ uniswapTrader.js
@@ -0,0 +1,13 @@
+const { ethers } = require('ethers')
+const { abi: IUniswapV3PoolABI } = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json')
+const { abi: SwapRouterABI} = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json')
+const { getPoolImmutables, getPoolState } = require('./helpers')
+const ERC20ABI = require('./abi.json')
+
+require('dotenv').config()
+
+const INFURA_URL_TESTNET = process.env.INFURA_URL_TESTNET
+const WALLET_ADDRESS = process.env.WALLET_ADDRESS
+const WALLET_SECRET = process.env.WALLET_SECRET
+
+const provider = new ethers.providers.JsonRpcProvider(INFURA_URL_TESTNET)
